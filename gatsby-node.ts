1168340b830b6e0b4c4c2ac9c55e18fcb1cec298
Tove Karlström <tovekarlstrom@hotmail.se>
2024-01-19 15:59:25 +0100

    fix: error trying to build project

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,16 +1,6 @@
 import { GatsbyNode } from "gatsby";
 import path from "path";
 
-interface ContentfulProjectsQuery {
-  allContentfulProjects: {
-    nodes: {
-      projectTitle: string;
-      slug: string;
-      projectCategory: string;
-    }[];
-  };
-}
-
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions;
   // Define a template for blog post
@@ -44,70 +34,4 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       });
     });
   }
-  //   const filter = result.data.allContentfulProjects.nodes;
-  //   if (posts.length > 0) {
-  //     posts.forEach((post: any) => {
-  //       createPage({
-  //         path: `/${post.projectCategory}/`,
-  //         component: category,
-  //         context: {
-  //           slug: post.projectCategory,
-  //         },
-  //       });
-  //     });
-  //   }
-  // };
-  // const createPages: GatsbyNode["createPages"] = async ({
-  //   graphql,
-  //   actions,
-  //   reporter,
-  // }) => {
-  //   const { createPage } = actions;
-  //   const blogPost = path.resolve("./src/templates/blog-post.tsx");
-  //   const category = path.resolve("./src/templates/blog-post.tsx");
-
-  //   const result = await graphql<ContentfulProjectsQuery>(`
-  //     {
-  //       allContentfulProjects {
-  //         nodes {
-  //           projectTitle
-  //           slug
-  //           projectCategory
-  //         }
-  //       }
-  //     }
-  //   `);
-
-  //   if (result.errors) {
-  //     reporter.panicOnBuild(
-  //       `There was an error loading your Contentful projects`,
-  //       result.errors
-  //     );
-  //     return;
-  //   }
-
-  //   const posts = result.data!.allContentfulProjects.nodes;
-  //   if (posts.length > 0) {
-  //     posts.forEach((post: any) => {
-  //       createPage({
-  //         path: `/${post.slug}/`,
-  //         component: blogPost,
-  //         context: {
-  //           slug: post.slug,
-  //         },
-  //       });
-  //     });
-  //   }
-  //   const filter = result.data!.allContentfulProjects.nodes;
-  //   if (posts.length > 0) {
-  //     posts.forEach((post: any) => {
-  //       createPage({
-  //         path: `/${post.projectCategory}/`,
-  //         component: category,
-  //         context: {
-  //           slug: post.projectCategory,
-  //         },
-  //       });
-  //     });
-  //   }
 };
--- src/pages/category.tsx
+++ src/pages/category.tsx
@@ -1,49 +1,49 @@
-// import React, { useState } from "react";
-// import { graphql } from "gatsby";
-// import Layout from "../components/layout";
+import React, { useState } from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
 
-// const Category = ({ data }: any) => {
-//   const dataResult = data.allContentfulProjects.nodes;
-//   const [categoryData, setCategoryData] = useState("Select category");
+const Category = ({ data }: any) => {
+  const dataResult = data.allContentfulProjects.nodes;
+  const [categoryData, setCategoryData] = useState("Select category");
 
-//   const allCategories = Array.from(
-//     new Set(dataResult.flatMap((post: any) => post.categories))
-//   );
+  const allCategories = Array.from(
+    new Set(dataResult.flatMap((post: any) => post.categories))
+  );
 
-//   const filteredPosts = dataResult.filter((post: any) => {
-//     if (categoryData !== "Select category") {
-//       return post.categories.includes(categoryData);
-//     }
-//   });
+  const filteredPosts = dataResult.filter((post: any) => {
+    if (categoryData !== "Select category") {
+      return post.categories.includes(categoryData);
+    }
+  });
 
-//   return (
-//     <Layout>
-//       <select onChange={(e) => setCategoryData(e.target.value)}>
-//         <option value="Select category">Select category</option>
-//         {allCategories.map((category: any, index) => (
-//           <option key={index} value={category}>
-//             {category}
-//           </option>
-//         ))}
-//       </select>
-//       <ul>
-//         {filteredPosts.map((item: any) => (
-//           <li key={item.id}>{item.projectTitle}</li>
-//         ))}
-//       </ul>
-//     </Layout>
-//   );
-// };
-// export const query = graphql`
-//   query MyQuery {
-//     allContentfulProjects {
-//       nodes {
-//         id
-//         categories
-//         slug
-//         projectTitle
-//       }
-//     }
-//   }
-// `;
-// export default Category;
+  return (
+    <Layout>
+      <select onChange={(e) => setCategoryData(e.target.value)}>
+        <option value="Select category">Select category</option>
+        {allCategories.map((category: any, index) => (
+          <option key={index} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+      <ul>
+        {filteredPosts.map((item: any) => (
+          <li key={item.id}>{item.projectTitle}</li>
+        ))}
+      </ul>
+    </Layout>
+  );
+};
+export const query = graphql`
+  query MyQuery {
+    allContentfulProjects {
+      nodes {
+        id
+        categories
+        slug
+        projectTitle
+      }
+    }
+  }
+`;
+export default Category;
